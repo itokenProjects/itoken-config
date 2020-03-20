24c610b3c6b7967c11e20cf6b17076f305a46a24
wzk-ai <18231378289@163.com>
2020-03-20 23:48:35 +0800

    update

--- src/main/java/com.gyf.itoken.config/ConfigApplication.java
+++ src/main/java/com.gyf.itoken.config/ConfigApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication  //开启springboot
-@EnableEurekaClient   //注册到eureka
-@EnableConfigServer  //开启分布式配置中心服务端
+@SpringBootApplication
+@EnableEurekaClient  //注册到eureka
+@EnableConfigServer //开启分布式配置中心服务
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class,args);
